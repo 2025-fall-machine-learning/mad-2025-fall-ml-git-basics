cacf9bf141557c1b92e8f88bb60138cf0085f193
EmogenR <emogen.rawls@gmail.com>
2025-10-30 11:20:30 -0500

    Update app_3_compute_e.py

--- bugs/app_3_compute_e.py
+++ bugs/app_3_compute_e.py
@@ -1,8 +1,12 @@
 def sum_one_to_million():
     """Manually add up numbers from 1 to 1,000,000."""
+
+    total = 0 # Moved initialized total to before the for loop
+
     for num_counter in range(1, 1000001):
-        total = 0 # Initialize total.
         total += num_counter
+
+    return total # Added return value
 
 
 def compute_e(precision=10):
@@ -28,7 +32,7 @@ def compute_e(precision=10):
     threshold = 10 ** (-(precision + 5))
 
     e = 1.0  # Start with 1/0! = 1
-    factorial = 1
+    factorial = 1 # Changed factorial to start at 1.
     n = 1
     while True:
         factorial *= n  # Compute n! incrementally. The factorials AFTER THE FIRST FACTORIAL are 1,
@@ -38,6 +42,7 @@ def compute_e(precision=10):
         if term < threshold:
             break
 
+        # Added addition.
         e += term
 
         n += 1
