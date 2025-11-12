744e931ad191576f9bba487efd6b3e13608258f5
Jeebjean <102182258+Jeebjean@users.noreply.github.com>
2025-11-12 15:19:23 -0600

    app_1 fixed

--- bugs/app_1_summer_picnic.py
+++ bugs/app_1_summer_picnic.py
@@ -6,11 +6,16 @@ def pick_the_winners(raffle_tickets):
     randomizer = random.Random(42)
     randomizer.shuffle(raffle_tickets)
     length = len(raffle_tickets)
+    # print(f"length: {length}")
     bottom_80_percent = int(length * 0.8)
+    print(f"bottom_80_percent: {bottom_80_percent}")
     top_20_percent = length - bottom_80_percent
-    eliminated = raffle_tickets[:bottom_80_percent]
-    keychain_winners = raffle_tickets[:top_20_percent]
-    top_3_winners = raffle_tickets[:3]
+    # print(f"top_20_percent: {top_20_percent}")
+    print(f"raffle_tickets: {raffle_tickets}")
+    # eliminated = raffle_tickets[top_20_percent:length]
+    # print(f"eliminated: {eliminated}")
+    keychain_winners = raffle_tickets[bottom_80_percent:]
+    top_3_winners = raffle_tickets[bottom_80_percent + 1:]
     print("Keychain winners are:", keychain_winners)
     print(f"Top 3 winners are: {top_3_winners}")
     print(f"Debugging... {raffle_tickets[0]} should not have won, and {raffle_tickets[-1]} should have won.")
